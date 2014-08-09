ae8ccf8b8db95286a6c1083b1d4ccd95c50c73dc
Jacek Radajewski <jacekrad@gmail.com>
2014-08-09 16:26:51 +1000

    1b finilised

--- prac02/exercise1b.py
+++ prac02/exercise1b.py
@@ -7,11 +7,20 @@ from sequence import *
 from prob import * 
 from symbol import * 
 from webservice import *
+from sys import stderr
 
 blosum62_background = readDistrib("blosum62.distrib")
 
 
 def get_distrib_from_fasta(fasta_filename):
+    '''
+    This function creates a distribution from a set sequences
+    read from a FASTA file.  All sequences are read and every
+    residue is used to construct the distribution.  This
+    function assumes that the FASTA file contains protein alphabet
+    sequence.  The function returns a Distrib object containing the
+    generated distribution.
+    '''
     protein_counts = {}
     # initilise the dictionary with zeros
     for residue in Protein_Alphabet.symbols:
@@ -30,3 +39,12 @@ hprd_background.writeDistrib("HPRD.distrib")
 
 print hprd_background
 print blosum62_background
+
+# compare the two distributions by calculating deltas for each amino acid
+sys.stderr.write("AA,HPRD,BLOSUM62,DELTA\n")
+for amino_acid in hprd_background.alpha:
+    delta = hprd_background[amino_acid] - blosum62_background[amino_acid]
+    sys.stderr.write(amino_acid + "," + str(hprd_background[amino_acid]) \
+                     + "," + str(blosum62_background[amino_acid]) + "," \
+                     + str(delta) + "\n")
+    
